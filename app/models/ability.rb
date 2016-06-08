d053ff6d19e2ed792cc0fb82b1f790ef4f579f72
Kristine Robison <kris.robison@oracle.com>
2016-06-07 18:58:41 -0700

    Add role configuration

---
+++ app/models/ability.rb
@@ -0,0 +1,28 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    elsif user.editor?
+      can :manage, Edition, state: :draft
+      can :manage, Flyby
+      can :manage, NewsStory
+      can :manage, OrbitDiagram
+      can :manage, Source
+      can :manage, Theme
+      can :read, SocialEngagement
+      can :read, User
+    elsif user.subscriber?
+      can :read, Edition, state: :published
+      can :share, Edition, state: :published
+      can :unsubscribe, User
+      can :create, Preference
+      can :update, Preference
+    else
+      can :read, Edition, state: :published
+      can :share, Edition, state: :published
+    end
+  end
+end
