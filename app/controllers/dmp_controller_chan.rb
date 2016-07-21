3bc06de04da0b4e94fecc2ba64e4a9142f4afc6a
Chan Kim <96chan.kim@gmail.com>
2016-07-20 18:10:03 -0700

    add preview as modal dialog and change share position to fit to what we have

---
+++ app/controllers/dmp_controller_chan.rb
@@ -0,0 +1,14 @@
+class DmpController < ApplicationController
+  respond_to :json, :html
+
+  def index
+  end
+
+  def draft
+  end
+
+  def testemail
+  	 UserMailer.daily_newspaper().deliver
+  	# UserMailer.verify_email().deliver
+  end
+end
