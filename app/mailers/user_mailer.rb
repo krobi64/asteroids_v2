130e33a19fdbf0743fc42a3342a60892efa39e0c
Raymond Xie <yuhua.xie@gmail.com>
2016-07-27 16:15:05 -0700

    rendered all fields on server side for daily newspaper email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,18 @@ class UserMailer < ActionMailer::Base
   
   # daily newspaper, should pass in a user object to this call
   def daily_newspaper(edition, email)
-    # @url = "http://localhost:3000/users/confirm"
     @edition = edition
-    mail( to: "yuhua.xie@gmail.com", subject: "Your Daily Minor Planet" )
+    @email = email
+
+    # 2016-07-23T07:00:00Z
+    # pDate = Date.strptime(edition.publish_date[0..10], "%Y-%m-%d")
+    pDate = edition.publish_date
+    @weekday = pDate.strftime('%A')
+    @year = pDate.strftime('%Y')
+    @month = pDate.strftime('%B')
+    @day = pDate.strftime('%-d')
+
+    mail( to: email, subject: "Your Daily Minor Planet" )
   end
 
   def daily_batch()
